0d3f4055d5701603a25e18ce3c3e2ebbdeada5bb
Anibal Juniper <redoubteifly@outlook.com>
2019-08-06 21:54:34 +0800

    updated

---
+++ Alioss.Client.Skin/Extend/AttrExtend.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Alioss.Client.Skin.Extend
+{
+    public class AttrExtend: DependencyObject
+    {
+        public static object GetSourceAttr(DependencyObject obj)
+        {
+            return (object)obj.GetValue(SourceAttrProperty);
+        }
+
+        public static void SetSourceAttr(DependencyObject obj, object value)
+        {
+            obj.SetValue(SourceAttrProperty, value);
+        }
+
+        /// <summary>
+        /// 附加图片
+        /// </summary>
+        public static readonly DependencyProperty SourceAttrProperty =
+            DependencyProperty.RegisterAttached("SourceAttr", typeof(object), typeof(AttrExtend), new PropertyMetadata(null));
+
+        /// <summary>
+        /// 字体大小
+        /// </summary>
+        public static readonly DependencyProperty TitleFontSizeProperty =
+           DependencyProperty.RegisterAttached("TitleFontSize", typeof(int), typeof(AttrExtend), new PropertyMetadata(null));
+
+        public static int GetTitleFontSize(DependencyObject obj)
+        {
+            return (int)obj.GetValue(TitleFontSizeProperty);
+        }
+    }
+}
---
+++ Alioss.Client.Skin/Extend/UniformGridRowColConver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Alioss.Client.Skin.Extend
+{
+    class UniformGridRowColConver : IValueConverter
+    {
+        public UniformGridRowColConver()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return 0;
+            }
+            int i = (value as IList).Count;
+            int row = i / 3;
+            int row_s = i % 3;
+            if (row_s > 0)
+            {
+                return row + 1;
+            }
+            else
+            {
+                return row;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
